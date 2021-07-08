153c25933a5a3ea5c7c864a64c36e8d6faf0f1e2
s1280078 <s1280078@u-aizu.ac.jp>
2021-07-08 15:58:39 +0900

    Won/lost message modified

--- Dice Game.c
+++ Dice Game.c
@@ -20,8 +20,9 @@ int main(){
   
     printf("Total value: %d\n", sum);
 
-    if(sum > 7) printf("you won!\n");
-    else printf("you lost.\n", name); 
+    if(sum > 7) printf("%s won!\n", name);
+    else printf("%s lost.\n", name);
+
   return 0;
 }
 
